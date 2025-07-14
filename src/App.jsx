c5e25e7cd78d64a006f2c6cadc53dea0c721cc65
https://github.com/Ritikagrawal07 <ritikkumar56678@gmail.com>
2025-07-14 20:00:56 +0530

    initial project setup commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>🌍 Tourist Attraction Finder</h1>
+    </div>
+  );
+};
+
+export default App;
